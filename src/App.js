43376ad83d90b6593fb16261dfadf77a2f892338
Tim Ludwig <tjtm09@yahoo.com>
2026-05-21 08:20:10 -0600

    Implement recipe counter with useReducer and add control buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+import { Button } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import RecipeForm from "./components/RecipeForm";
 import RecipeList from "./components/RecipeList";
@@ -10,6 +11,23 @@ function App() {
   const [loader, setLoader] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
   const [modal, setModal] = useState(false);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "increment":
+        return { count: state.count + 1 };
+      case "decrement":
+        return { count: state.count - 1 };
+      case "reset":
+        return { count: 0 };
+      default:
+        return state;
+    }
+  };
+
+  const initialState = {
+    count: 0,
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggle = () => setModal(!modal);
 
@@ -21,8 +39,16 @@ function App() {
     }, 5000);
   };
 
+  useEffect(() => {
+    console.log("Recipes updated:", recipes);
+  }, [recipes]);
+
   return (
     <>
+      <p>Count:{state.count}</p>
+      <Button onClick={() => dispatch({ type: "increment" })}>Increment</Button>
+      <Button onClick={() => dispatch({ type: "decrement" })}>Decrement</Button>
+      <Button onClick={() => dispatch({ type: "reset" })}>Reset</Button>
       <RecipeForm addRecipe={addRecipe} Loader={loader} />
       <RecipeList
         recipes={recipes}
